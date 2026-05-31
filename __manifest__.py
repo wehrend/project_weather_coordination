45002c42fa10aa4edf4aeba7abac7d4cd78b1330
Sven Wehrend <swehrend@ant-neuro.com>
2026-05-31 21:37:29 +0200

    fix xml view files

--- __manifest__.py
+++ __manifest__.py
@@ -4,10 +4,10 @@
     'version': '1.0',
     'summary': 'Automatically block or unblock tasks based on the client\'s local weather forecast.',
     'category': 'Services/Project',
-    'author': 'Dein Name',
+    'author': 'Sven Wehrend',
     'depends': ['project', 'base'],
     'data': [
-        'data/ir_cron_data.xml',
+        # 'data/ir_cron_data.xml',
         'views/project_task_views.xml',
     ],
     'installable': True,
